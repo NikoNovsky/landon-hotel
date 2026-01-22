a1ab738bb174b0555550c881400aec87ffd0ff18
NikoNovsky <nikonovsky@gmail.com>
2026-01-22 14:08:08 +0100

    Room class fixes

--- src/main/java/com/example/landonhotel/data/entity/Room.java
+++ src/main/java/com/example/landonhotel/data/entity/Room.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.ToString;
 
-import java.util.UUID;
-
 @Entity
 @Table(name = "rooms")
 @Data
@@ -24,11 +22,4 @@ public class Room {
 
     @Column(name="bed_info")
     private String bedInfo;
-
-    public Room(int id, String name, String roomNumber, String bedInfo) {
-        this.id = id;
-        this.name = name;
-        this.roomNumber = roomNumber;
-        this.bedInfo = bedInfo;
-    }
 }
