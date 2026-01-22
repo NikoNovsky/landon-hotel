05009ee14716fec94ff1597325aee89962422657
NikoNovsky <nikonovsky@gmail.com>
2026-01-22 12:30:24 +0100

    created room entity

---
+++ src/main/java/com/example/landonhotel/data/entity/Room.java
@@ -0,0 +1,34 @@
+package com.example.landonhotel.data.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "rooms")
+@Data
+@ToString
+public class Room {
+    @Id
+    @Column(name="room_id")
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    private long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="room_number")
+    private String number;
+
+    @Column(name="bed_info")
+    private String bedInfo;
+
+    public Room(int id, String name, String number, String bedInfo) {
+        this.id = id;
+        this.name = name;
+        this.number = number;
+        this.bedInfo = bedInfo;
+    }
+}
