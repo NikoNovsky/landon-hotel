eb5db2cca6525f22216c9acbfbeb74eae71f4218
NikoNovsky <nikonovsky@gmail.com>
2026-01-23 23:32:35 +0100

    created ReservationRepository

---
+++ src/main/java/com/example/landonhotel/data/repository/ReservationRepository.java
@@ -0,0 +1,11 @@
+package com.example.landonhotel.data.repository;
+
+import com.example.landonhotel.data.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Date;
+import java.util.Optional;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    Optional<Reservation> findByReservationDate(Date date);
+}
