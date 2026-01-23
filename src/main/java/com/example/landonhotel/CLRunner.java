881e0b92c7b1d3f86bc2e91b4eef9c01c12f8ad3
NikoNovsky <nikonovsky@gmail.com>
2026-01-23 23:43:19 +0100

    CLRunner updated

--- src/main/java/com/example/landonhotel/CLRunner.java
+++ src/main/java/com/example/landonhotel/CLRunner.java
@@ -1,9 +1,15 @@
 package com.example.landonhotel;
 
+import com.example.landonhotel.data.entity.Guest;
+import com.example.landonhotel.data.entity.Reservation;
 import com.example.landonhotel.data.entity.Room;
+import com.example.landonhotel.data.repository.GuestRepository;
+import com.example.landonhotel.data.repository.ReservationRepository;
 import com.example.landonhotel.data.repository.RoomRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,16 +17,30 @@ import java.util.Optional;
 public class CLRunner implements CommandLineRunner {
 
     private final RoomRepository roomRepository;
+    private final GuestRepository guestRepository;
+    private final ReservationRepository reservationRepository;
 
-    public CLRunner(RoomRepository roomRepository) {
+    public CLRunner(RoomRepository roomRepository, GuestRepository guestRepository, ReservationRepository reservationRepository) {
         this.roomRepository = roomRepository;
+        this.guestRepository = guestRepository;
+        this.reservationRepository = reservationRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        List<Room> rooms = this.roomRepository.findAll();
-        Optional<Room> room = this.roomRepository.findByRoomNumberIgnoreCase("p1");
-        System.out.println(room);
-        rooms.forEach(System.out::println);
+//        List<Room> rooms = this.roomRepository.findAll();
+//        Optional<Room> room = this.roomRepository.findByRoomNumberIgnoreCase("p1");
+//        System.out.println(room);
+//        rooms.forEach(System.out::println);
+
+        List<Guest> guests = this.guestRepository.findAll();
+        Optional<Guest> guest = this.guestRepository.findGuestById(1);
+        System.out.println(guest);
+        guests.forEach(System.out::println);
+
+        List<Reservation> reservations = this.reservationRepository.findAll();
+        Optional<Reservation> reservation = this.reservationRepository.findByReservationDate(new Date(2023, 7, 28));
+        System.out.println(guest);
+        guests.forEach(System.out::println);
     }
 }
