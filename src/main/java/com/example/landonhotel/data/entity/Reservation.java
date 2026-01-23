86a9d1ecd627908a561684205dea7a568596dff3
NikoNovsky <nikonovsky@gmail.com>
2026-01-23 23:25:02 +0100

    created reservation entity

---
+++ src/main/java/com/example/landonhotel/data/entity/Reservation.java
@@ -0,0 +1,28 @@
+package com.example.landonhotel.data.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "reservations")
+@Data
+@ToString
+public class Reservation {
+
+    @Id
+    @Column(name = "reservation_id")
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    private long reservationId;
+
+    @Column(name = "room_id")
+    private long roomId;
+
+    @Column(name = "guest_id")
+    private long guestId;
+
+    @Column(name = "res_date")
+    private Date resDate;
+}
