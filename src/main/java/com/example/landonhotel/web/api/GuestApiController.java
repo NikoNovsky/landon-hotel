04704614bde2fbb2a2308a3511abafec2ca64c7b
NikoNovsky <nikonovsky@gmail.com>
2026-01-25 21:37:54 +0100

    GuestApiController updated

--- src/main/java/com/example/landonhotel/web/api/GuestApiController.java
+++ src/main/java/com/example/landonhotel/web/api/GuestApiController.java
@@ -1,4 +1,56 @@
 package com.example.landonhotel.web.api;
 
+import com.example.landonhotel.data.entity.Guest;
+import com.example.landonhotel.data.repository.GuestRepository;
+import com.example.landonhotel.web.exception.BadRequestException;
+import com.example.landonhotel.web.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/guest")
 public class GuestApiController {
+
+    private final GuestRepository guestRepository;
+
+    public GuestApiController(GuestRepository guestRepository) {
+        this.guestRepository = guestRepository;
+    }
+
+    @GetMapping
+    public List<Guest> getAllGuests() {
+        return this.guestRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Guest createGuest(Guest guest) {
+        return this.guestRepository.save(guest);
+    }
+
+    @GetMapping("/{id}")
+    public Guest getGuest(@PathVariable("id") long id) {
+        Optional<Guest> guest = this.guestRepository.findById(id);
+        if (guest.isEmpty()) {
+            throw new NotFoundException("Guest with id " + id + " not found");
+        }
+        return guest.get();
+    }
+
+    @PutMapping("/{id}")
+    public Guest updateGuest(@PathVariable("id") long id, @RequestBody Guest guest) {
+        if (id != guest.getId()) {
+            throw new BadRequestException("id on path does not match body");
+        }
+        return  this.guestRepository.save(guest);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.RESET_CONTENT)
+    public void deleteGuest(@PathVariable("id") long id) {
+        this.guestRepository.deleteById(id);
+    }
 }
