d916cb420715dfb972b90628fa12f7ba186a5a80
NikoNovsky <nikonovsky@gmail.com>
2026-01-27 13:51:27 +0100

    copied from landon-hotel

---
+++ src/test/java/com/example/nikshotel/NiksHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nikshotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiksHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
