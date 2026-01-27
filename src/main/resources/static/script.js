83d8ad2c200430117b109fade3ae3e6c62d3b6ce
NikoNovsky <nikonovsky@gmail.com>
2026-01-27 23:53:01 +0100

    added put, delete and post methods to room-list

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -33,6 +33,13 @@ const bootstrapScript = document.createElement("script");
                         tr.appendChild(fourthColumn);
 
                         tr.addEventListener("click", () => {
+                            const nameUpdateInput = document.querySelector('input[name="nameUpdate"]');
+                            const roomNumberUpdate = document.querySelector('input[name="roomNumberUpdate"]');
+                            const bedInfoUpdate = document.querySelector('input[name="bedInfoUpdate"]');
+
+                            nameUpdateInput.value = room.name;
+                            roomNumberUpdate.value = room.roomNumber;
+                            bedInfoUpdate.value = room.bedInfo;
                             radio.checked = true;
                         });
 
@@ -41,6 +48,62 @@ const bootstrapScript = document.createElement("script");
                 })
                 .catch(err => console.error("Error loading all rooms:", err));
         });
+
+        document.getElementById('addRoomForm').addEventListener('submit', e => {
+            e.preventDefault()
+
+            fetch('/api/rooms', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: e.target.name.value,
+                    roomNumber: e.target.roomNumber.value,
+                    bedInfo: e.target.bedInfo.value
+                })
+            })
+            location.reload();
+        })
+
+        document.getElementById('updateRoomForm').addEventListener('submit', e => {
+            e.preventDefault()
+
+            const selected = document.querySelector('input[name="selectedRoom"]:checked');
+            const id = selected?.id ? selected.id : null;
+
+            fetch(`/api/rooms/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: id,
+                    name: e.target.nameUpdate.value,
+                    roomNumber: e.target.roomNumberUpdate.value,
+                    bedInfo: e.target.bedInfoUpdate.value
+                })
+            })
+            location.reload();
+        })
+
+        document.getElementById('deleteRoomButton').addEventListener('click', e => {
+            e.preventDefault()
+
+            const selected = document.querySelector('input[name="selectedRoom"]:checked');
+            const id = selected?.id ? selected.id : null;
+
+            fetch(`/api/rooms/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: id
+                })
+            })
+            location.reload();
+        })
     };
 
     document.body.appendChild(bootstrapScript);
